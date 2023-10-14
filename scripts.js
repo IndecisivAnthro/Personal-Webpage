e7adce9a5a400d568e275506f54c34b9b2bcf330
Gavin Babcock <147926673+IndecisivAnthro@users.noreply.github.com>
2023-10-14 15:47:26 -0230

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
+// Prompt the user for their name
+var userName = prompt("What is your name?");
 
+// Check if the user entered a name
+if (userName) {
+    // Create a greeting message
+    var greeting = "Hello, " + userName + "! Welcome to the world of JavaScript!";
+
+    // Display the greeting message
+    alert(greeting);
+} else {
+    // If the user didn't enter a name, display a generic greeting
+    alert("Hello! Welcome to the world of JavaScript!");
+}
+<script>
+    let name = "Craig";
+
+    if (name === Craig) {
+        console.log("Hi Crag");
+    } else { 
+    console.log("wait, where's Craig?");
+    }
+
+</script>
